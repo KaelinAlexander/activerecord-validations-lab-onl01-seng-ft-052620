80c5726ca3c310a011e74202dadddb339b44f426
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-03 14:41:46 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,8 +1,8 @@
 class TitleValidator < ActiveModel::Validator
 
   def validate(record)
-    unless record.include?("Won't Believe") || record.include?("Secret") || record.include?("Top [number]") || record.include?("Guess")
-    record.errors[:title] << "Please make this more clickbait-y."
+      unless record.include?("Won't Believe") || record.include?("Secret") || record.include?("Top [number]") || record.include?("Guess")
+      record.errors[:title] << "Please make this more clickbait-y."
+      end
   end
-
 end

30612bab7978d5c6eacd607ee32cd17727bd357e
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-03 15:18:08 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,9 @@
+require 'pry'
+
 class TitleValidator < ActiveModel::Validator
 
   def validate(record)
-      byebug
+      binding.pry
       unless record["Won\'t Believe"] || record["Secret"] || record["Top [number]"] || record["Guess"]
       record.errors[:title] << "Please make this more clickbait-y."
       end

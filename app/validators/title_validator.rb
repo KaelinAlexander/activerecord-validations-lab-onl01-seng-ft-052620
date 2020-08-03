ddbba03f71a99b767bcfac9a25eda8b08a65cc3f
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-03 14:56:06 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,7 @@
 class TitleValidator < ActiveModel::Validator
 
   def validate(record)
-      unless record["Won\'\t Believe"] || record["Secret"] || record["Top [number]"] || record["Guess"]
+      unless record["Won\'t Believe"] || record["Secret"] || record["Top [number]"] || record["Guess"]
       record.errors[:title] << "Please make this more clickbait-y."
       end
   end

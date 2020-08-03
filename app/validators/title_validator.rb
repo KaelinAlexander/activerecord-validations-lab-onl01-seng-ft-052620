5a5b625e9a8c062dcae178232759636b1b49ed28
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-08-03 15:22:32 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -3,7 +3,7 @@ require 'pry'
 class TitleValidator < ActiveModel::Validator
 
   def validate(record)
-      unless record.title["Won't Believe"] || record.title["Secret"] || record.title["Top [number]"] || record.title["Guess"]
+      unless record.title.include? "Won't Believe" || record.title.include? "Secret" || record.title.include? "Top [number]" || record.include? "Guess"
       record.errors[:title] << "Please make this more clickbait-y."
       end
   end
